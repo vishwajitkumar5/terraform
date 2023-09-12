379598921e58039dbfc39b6161b347efb1342dbe
vishawjit <vishawjitkumar@gmail.com>
2023-09-12 14:40:07 +0530

    first terraform

--- Day 1/main.tf
+++ Day 1/main.tf
@@ -1,9 +1,14 @@
    provider "aws" {
      region = "us-west-2"  # Set your desired AWS region
    }
+
+
+
    resource "aws_instance" "example" {
      ami           = "ami-03f65b8614a860c29"  # Specify an appropriate AMI ID
      instance_type = "t2.micro"
      subnet_id = "subnet-0c5ca548286b6c0d0"
-     tags = "Demo_ec2"
+     tags = {
+       Name = "Demo_ec2"
+     }
    }
