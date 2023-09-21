7a4be065f4ac7179d600fd8aa64c9ab883658da3
vishwajit kumar <vishawjitkumar@gmail.com>
2023-09-21 11:29:28 +0530

    daye 5

---
+++ Day5/main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+    region = us-west-2
+  
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-03f65b8614a860c29"  # Specify an appropriate AMI ID
+    instance_type = "t2.micro"
+  
+}
