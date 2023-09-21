af70f9effd6183c40e6f6f1e2c7cb3be1da8f558
vishwajit kumar <vishawjitkumar@gmail.com>
2023-09-21 12:43:28 +0530

    daye 5

--- Day5/main.tf
+++ Day5/main.tf
@@ -13,3 +13,14 @@ resource "aws_s3_bucket" "s3_bucket" {
     bucket = "vishwajit-demo-mybucket"
     
 }
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name           = "terraform-lock"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
