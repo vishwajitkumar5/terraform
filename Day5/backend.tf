aba21fb79400d369c662d739fb6ed964de791113
vishwajit kumar <vishawjitkumar@gmail.com>
2023-09-22 20:18:38 +0530

    Day5

--- Day5/backend.tf
+++ Day5/backend.tf
@@ -1,8 +1,9 @@
-/*terraform {
+terraform {
   backend "s3" {
     bucket = "vishwajit-demo-mybucket"
     region = "us-west-2"
     key = "vshwajit/terraform.tfstate"
-    
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
   }
-}*/
+}
